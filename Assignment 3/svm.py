3fcc2e1e9e7afefa0d81a7cc098f5857793fef6f
knisar <knisar17@gmail.com>
2018-05-10 23:40:06 -0400

    code

---
+++ Assignment 3/svm.py
@@ -0,0 +1,126 @@
+from sklearn.svm import SVC
+
+import numpy as np
+from scipy.io import loadmat
+from scipy.optimize import minimize
+def preprocess():
+    """ 
+     Input:
+     Although this function doesn't have any input, you are required to load
+     the MNIST data set from file 'mnist_all.mat'.
+
+     Output:
+     train_data: matrix of training set. Each row of train_data contains 
+       feature vector of a image
+     train_label: vector of label corresponding to each image in the training
+       set
+     validation_data: matrix of training set. Each row of validation_data 
+       contains feature vector of a image
+     validation_label: vector of label corresponding to each image in the 
+       training set
+     test_data: matrix of training set. Each row of test_data contains 
+       feature vector of a image
+     test_label: vector of label corresponding to each image in the testing
+       set
+    """
+
+    mat = loadmat('mnist_all.mat')  # loads the MAT object as a Dictionary
+
+    n_feature = mat.get("train1").shape[1]
+    n_sample = 0
+    for i in range(10):
+        n_sample = n_sample + mat.get("train" + str(i)).shape[0]
+    n_validation = 1000
+    n_train = n_sample - 10 * n_validation
+
+    # Construct validation data
+    validation_data = np.zeros((10 * n_validation, n_feature))
+    for i in range(10):
+        validation_data[i * n_validation:(i + 1) * n_validation, :] = mat.get("train" + str(i))[0:n_validation, :]
+
+    # Construct validation label
+    validation_label = np.ones((10 * n_validation, 1))
+    for i in range(10):
+        validation_label[i * n_validation:(i + 1) * n_validation, :] = i * np.ones((n_validation, 1))
+
+    # Construct training data and label
+    train_data = np.zeros((n_train, n_feature))
+    train_label = np.zeros((n_train, 1))
+    temp = 0
+    for i in range(10):
+        size_i = mat.get("train" + str(i)).shape[0]
+        train_data[temp:temp + size_i - n_validation, :] = mat.get("train" + str(i))[n_validation:size_i, :]
+        train_label[temp:temp + size_i - n_validation, :] = i * np.ones((size_i - n_validation, 1))
+        temp = temp + size_i - n_validation
+
+    # Construct test data and label
+    n_test = 0
+    for i in range(10):
+        n_test = n_test + mat.get("test" + str(i)).shape[0]
+    test_data = np.zeros((n_test, n_feature))
+    test_label = np.zeros((n_test, 1))
+    temp = 0
+    for i in range(10):
+        size_i = mat.get("test" + str(i)).shape[0]
+        test_data[temp:temp + size_i, :] = mat.get("test" + str(i))
+        test_label[temp:temp + size_i, :] = i * np.ones((size_i, 1))
+        temp = temp + size_i
+
+    # Delete features which don't provide any useful information for classifiers
+    sigma = np.std(train_data, axis=0)
+    index = np.array([])
+    for i in range(n_feature):
+        if (sigma[i] > 0.001):
+            index = np.append(index, [i])
+    train_data = train_data[:, index.astype(int)]
+    validation_data = validation_data[:, index.astype(int)]
+    test_data = test_data[:, index.astype(int)]
+
+    # Scale data to 0 and 1
+    train_data /= 255.0
+    validation_data /= 255.0
+    test_data /= 255.0
+
+    return train_data, train_label, validation_data, validation_label, test_data, test_label
+
+# import numpy as np
+train_data, train_label, validation_data, validation_label, test_data, test_label = preprocess()
+
+# number of classes
+n_class = 10
+
+# number of training samples
+n_train = train_data.shape[0]
+
+# number of features
+n_feature = train_data.shape[1]
+Y = np.zeros((n_train, n_class))
+
+
+W_b = np.zeros((n_feature + 1, n_class))
+initialWeights_b = np.zeros((n_feature + 1, n_class))
+opts_b = {'maxiter': 1}
+
+args_b = (train_data, Y)
+
+# train_data = train_data[:100000,]
+# train_label = train_label[:100000,]
+
+train_label = train_label.ravel()
+validation_label = validation_label.ravel()
+test_label = test_label.ravel()
+
+clf = SVC(C = 20)
+clf.fit(train_data, train_label)
+print("C is " + str(20))
+predicted_label = clf.predict(train_data)
+# Find the accuracy on Training Dataset
+print('\n Training set Accuracy:' + str(100 * np.mean((predicted_label == train_label).astype(float))) + '%')
+
+# Find the accuracy on Validation Dataset
+predicted_label = clf.predict(validation_data)
+print('\n Validation set Accuracy:' + str(100 * np.mean((predicted_label == validation_label).astype(float))) + '%')
+
+# Find the accuracy on Testing Dataset
+predicted_label = clf.predict(test_data)
+print('\n Testing set Accuracy:' + str(100 * np.mean((predicted_label == test_label).astype(float))) + '%')
